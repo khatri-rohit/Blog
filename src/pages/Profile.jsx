eeeb741fa6ee242aef2d0380d58f156592aea630
Rohit <rohitkhatri111112@gmail.com>
2024-10-02 02:02:42 +0530

    Delete Bookmarks

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -11,6 +11,7 @@ import useUsers from "../context/User";
 import toast, { Toaster } from "react-hot-toast";
 import { BiDotsHorizontalRounded } from "react-icons/bi";
 import DropDown from "../utils/DropDown";
+import PostDropDown from "../utils/PostDropDown";
 
 
 const Profile = () => {
@@ -385,20 +386,59 @@ const Bookmarks = () => {
   const { id } = useParams()
   const [bookMarks, setBookMarks] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [curDrop, setCurDrop] = useState('');
 
-  useEffect(() => {
-    (async () => {
+  const AllBookmarks = async () => {
+    try {
       setLoading(true);
       const { data } = await supabase
         .from('bookmarks')
         .select()
         .eq('username', id);
       setLoading(false);
+      console.log(data);
       if (data)
         setBookMarks(data);
-    })();
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
+  useEffect(() => {
+    AllBookmarks();
   }, [])
+
+  const deleteBookmark = async (postId) => {
+    console.log(postId);
+    await supabase
+      .from('bookmarks')
+      .delete()
+      .eq('post_id', postId);
+    toast('Bookmark Deleted', {
+      duration: 100,
+      position: 'top-right',
+
+      // Styling
+      style: { padding: '1rem 1.5rem' },
+      className: 'font-bold',
+
+      // Custom Icon
+      icon: '✅',
+
+      // Change colors of success/error/loading icon
+      iconTheme: {
+        primary: '#000',
+        secondary: '#fff',
+      },
+
+      // Aria
+      ariaProps: {
+        role: 'alert',
+        'aria-live': 'polite',
+      },
+    });
+    AllBookmarks();
+  }
 
   return (
     <div className="p-3">
@@ -409,7 +449,7 @@ const Bookmarks = () => {
           {
             loading &&
             // Loading
-            <div className="absolute bg-gray-400 bg-opacity-60 h-full w-full z-10 flex items-center justify-center">
+            <div className="absolute bg-transparent bg-opacity-60 h-full w-full z-10 flex items-center justify-center">
               <ClipLoader />
             </div>
           }
@@ -417,35 +457,53 @@ const Bookmarks = () => {
             {
               bookMarks?.length === 0 ?
                 (loading || <p className="text-xl">You haven't Saved any posts</p>)
-                : <div className={`grid grid-cols-2 grid-rows-7 gap-4 ms-0.5 w-full`}>
-                  {/* : <div className={`flex flex-nowrap ms-0.5`}> */}
-                  {
-                    (bookMarks?.map((post, _) => (
-                      <NavLink to={`/post/${post?.post_id}`} key={_}
-                        className="border-r p-2 bg-white mr-2 drop-shadow-lg rounded-lg cursor-pointer w-full">
-                        <div className="w-96 max-w-xs overflow-hidden transition-shadow duration-300 ease-in-out">
-                          <div className="ml-1">
-                            <img src={post?.cover_img}
-                              className="object-cover w-full rounded-xl h-[25vh]"
-                              alt={post?.blog_title} />
+                // : <div className={`grid grid-cols-2 grid-rows-7 gap-4 ms-0.5 w-full`}>
+                : <div className={`flex flex-nowrap ms-0.5`}>                  {
+                  (bookMarks?.map((post, _) => (
+                    <div key={_}
+                      className="border-r p-2 bg-white drop-shadow-lg rounded-lg cursor-pointer w-full gap-3">
+                      <div className="max-w-xs overflow-hidden transition-shadow duration-300 ease-in-out">
+                        <NavLink to={`/post/${post?.post_id}`}>
+                          <img src={post?.cover_img}
+                            className="object-cover w-full rounded-xl h-[25vh]"
+                            alt={post?.blog_title} />
+                        </NavLink>
+                        <div className="pt-2 px-2 flex justify-between items-start">
+                          <p className="text-pretty font-semibold text-black">
+                            {post?.blog_title}
+                          </p>
+                          <div className="relative ms-3">
+                            <button
+                              className="absolute right-0"
+                              onClick={() => setCurDrop(post.id)}>
+                              <BiDotsHorizontalRounded className="text-3xl" />
+                              {
+                                curDrop === post.id && <PostDropDown curPost={curDrop}
+                                  postid={post?.id} setCurPost={setCurDrop} size={"w-[8rem]"}>
+                                  <button onClick={() => {
+                                    deleteBookmark(post.post_id);
+                                    setCurDrop('');
+                                  }
+                                  }>
+                                    Delete
+                                  </button>
+                                </PostDropDown>
+                              }
+                            </button>
                           </div>
-                          <div className="pt-2 px-2">
-                            <p className="text-pretty font-semibold text-black">
-                              {post?.blog_title}
-                            </p>
-                          </div>
-                          <div className="px-2"
-                            dangerouslySetInnerHTML={{ __html: (post.summary).substring(0, 80) + "..." }} />
                         </div>
-                      </NavLink>
-                    )))
-                  }
+                        <div className="px-2"
+                          dangerouslySetInnerHTML={{ __html: (post.summary).substring(0, 80) + "..." }} />
+                      </div>
+                    </div>
+                  )))
+                }
                 </div>
             }
           </div>
         </div>
       </div>
-    </div>
+    </div >
   )
 
 }
---
+++ src/utils/PostDropDown.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useRef } from "react";
+
+const PostDropDown = ({ children, size, curPost, setCurPost, postid }) => {
+    const dropRef = useRef(null);
+
+    useEffect(() => {
+        const clickOutside = (e) => {
+            if (dropRef.current && !dropRef.current.contains(e.target)) {
+                setCurPost('');
+            }
+        };
+        window.addEventListener("mousedown", clickOutside);
+        return () => window.removeEventListener("mousedown", clickOutside);
+    }, [dropRef, curPost]);
+
+    return (
+        <>
+            {curPost === postid && (
+                <div
+                    ref={dropRef}
+                    className={`shadows flex flex-col items-start absolute -right-2 top-[1.5rem] px-3 py-1 bg-gray-200 ${size}`}>
+                    {children}
+                </div>
+            )}
+        </>
+    );
+};
+
+export default PostDropDown;
